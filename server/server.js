c49eec13864cad63fab1b0e7aba4897f74ddc8ed
tafveezA15 <tafveez.ahmad@klipit.co>
2023-07-07 16:01:01 +0400

    updated server.js with findbyidandupdate

--- server/server.js
+++ server/server.js
@@ -15,12 +15,23 @@ app.get('/blog',(req,res)=>{
 })
 app.get('/products',async(req,res)=>{
 try{
-const products = Product.find({})
+const products = await Product.find({})
  res.status(200).json(products)
 }catch(error){
-    res.status(500).json({message:error.message})
+ res.status(500).json({message:error.message})
     
 }
+})
+app.get('products/:id',async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id)
+        res.status(200).json(product)
+
+    }catch(error){
+        res.status(500).json({message:error.message})
+
+    }
 })
 app.post('/products',async(req,res) =>{
     // console.log(res.body);
@@ -33,6 +44,22 @@ app.post('/products',async(req,res) =>{
      res.status(500).json({message:error.message})
     }
 })
+
+// update by id
+app.put('products/:id',async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdandUpdate(id,req.body.params)
+        if(product ==nil){
+        res.status(404).json({message:'error in response for findByIdAndUpdate'})
+        }
+        }
+        catch(error){
+        res.status(500).json({message:error.message})
+
+    }
+})
+
 mongoose.set("strictQuery",false)
 mongoose.connect('mongodb+srv://tafveezahmad:z4Y2YeseDNJ1YOR6@cluster0.fqqtjkb.mongodb.net/?retryWrites=true&w=majority').then(
     ()=>{
