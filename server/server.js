7e946502e27dc736b9030377cd42ae98bf72f3d5
tafveezA15 <tafveez.ahmad@klipit.co>
2023-07-10 10:21:20 +0400

    updated with routes

--- server/server.js
+++ server/server.js
@@ -1,82 +1,21 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = express()
 const Product = require('./models/productModel.js')
+dotenv.config({path:'./config/config.env'})
+const PORT = process.env.PORT ||5000
+const NODE_ENV =process.env.NODE_ENV
 
 app.use(express.json())
 
 //setting middleware
 app.use(express.urlencoded({extended:false}))
 
-// routes
-app.get('/', (req,res)=>{
-res.send("Hello Node API")
-})
-
-app.get('/blog',(req,res)=>{
-    res.send('hello tafveez is here')
-})
-app.get('/products',async(req,res)=>{
-try{
-const products = await Product.find({})
- res.status(200).json(products)
-}catch(error){
- res.status(500).json({message:error.message})
-    
-}
-})
-app.get('/products/:id',async(req,res)=>{
-    try{
-        const {id} = req.params;
-        const product = await Product.findById(id)
-        res.status(200).json(product)
-
-    }catch(error){
-        res.status(500).json({message:error.message})
-
-    }
-})
-app.post('/products',async(req,res) =>{
-    // console.log(res.body);
-    // res.send(req.body)
-    try{
-        const product = await Product.create(req.body)
-        res.status(200).json(product)
-     
-    }catch(error){
-     res.status(500).json({message:error.message})
-    }
-})
-
-// update by id
-app.put('/products/:id',async(req,res)=>{
-    try{
-        const {id} = req.params
-        const product = await Product.findByIdAndUpdate(id,req.body)
-        if(!product){
-       return  res.status(404).json({message:`can't find any item with give id ${id}`})
-        }
-        const updatedProduct = await Product.findById(id)
-        res.status(200).json(updatedProduct)
-        }
-        catch(error){
-        res.status(500).json({message:error.message})
-
-    }
-})
-
-//delete product
-app.delete('/products/:id',async(req,res)=>{
-    try{const {id} = req.params
-    const product = await Product.findByIdAndDelete(id,req.body)
-    if(!product){
-       return  res.status(404).json({message:`can't find the item with given id ${id}`})
-    }
-     res.status(200).json(product)
-    }catch(error){
-        res.status(500).json({message:error.message})
-    }
-    })
+// route file
+const products = require('./routes/product.js')
+// mount routers
+app.use('/api/v1/products',products)
 
 
 
@@ -85,8 +24,8 @@ mongoose.set("strictQuery",false)
 mongoose.connect('mongodb+srv://tafveezahmad:z4Y2YeseDNJ1YOR6@cluster0.fqqtjkb.mongodb.net/?retryWrites=true&w=majority').then(
     ()=>{
         console.log('connected to mongodb')
-        app.listen(3000,()=>{
-            console.log('Node API app is runnning on port :3000')
+        app.listen(PORT,()=>{
+            console.log(`Node API app is runnning on ${NODE_ENV} enviornment port :${PORT}`)
         })
      
     }
