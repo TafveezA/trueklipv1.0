0788f90ebf0eb9fcaa8920d2448bafb0238dc979
tafveezA15 <tafveez.ahmad@klipit.co>
2023-07-11 15:23:38 +0400

    modified QR code logic

--- blockchain/contracts/Validation.sol
+++ blockchain/contracts/Validation.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.9;
 contract Validation{
 
-mapping (uint256 =>bytes32) hashByKlipId;
-function hashData( uint256 _klipId,string memory _batchNumber,uint256 _mfgDate, uint256 _expiryDate,uint256 _warranty) public  returns  (bytes32)
+mapping(uint256 =>mapping(bytes32=>bool)) hashByKlipId;
+function hashData( uint256 _klipId,string memory _batchNumber,uint256 _mfgDate, uint256 _expiryDate,uint256 _warranty,string memory ) public  returns  (bytes32)
 {
-    
-    return hashByKlipId[_klipId] = keccak256(abi.encodePacked(_batchNumber,_mfgDate,_expiryDate,_warranty));
+    bytes32 _hash=keccak256(abi.encodePacked(_batchNumber,_mfgDate,_expiryDate,_warranty));
+    hashByKlipId[_klipId][_hash] =true ;
+    return _hash;
 }
 
-function getHashById(uint256 _klipId) public view returns (bytes32)
+function getHashById(uint256 _klipId, bytes32 _hashed) public view returns (bool)
 {
-return hashByKlipId[_klipId];
+return hashByKlipId[_klipId][_hashed] == true;
 }
 }
