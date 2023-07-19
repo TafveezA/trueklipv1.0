b0488967e1c85670fe791e41ff5708efa555e40a
TafveezA <tafveez.alphadev@gmail.com>
2023-07-19 18:08:43 +0400

    updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex-shrink-0">
-          <a href="/tracking" className="text-white font-bold text-lg">
+          <a href="/" className="text-white font-bold text-lg">
   <img src={logo} alt="Logo" className="h-10 w-10" />
 </a>
           </div>
