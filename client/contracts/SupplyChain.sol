ae8bf50fd252817db1e8a12abdc8e94aeae85c05
TafveezA <tafveez.alphadev@gmail.com>
2023-08-04 11:22:52 +0400

    updates in Supplychain.sol

--- client/contracts/SupplyChain.sol
+++ client/contracts/SupplyChain.sol
@@ -19,8 +19,8 @@ function registerProducer(uint256 _producerId,address _producerAddress)onlyAdmin
    registeredProducer[_producerId][_producerAddress] =true;
 }
 
-function registerRetailer(uint256 _producerId,address _retailerAddress)onlyAdmin public {
-   registeredRetailer[_producerId][_retailerAddress] =true;
+function registerRetailer(uint256 _retailerId,address _retailerAddress)onlyAdmin public {
+   registeredRetailer[_retailerId][_retailerAddress] =true;
 }
 
 
@@ -30,10 +30,12 @@ struct Product{
     string certificate;
     string batchNumber;
     string productionData;
-    string truklipId;
+    uint256 truklipId;
     bytes32 uriInfo;
     address producerAddress;
     uint256 producerId;
+    Retailer retailerDetail;
+    Customer customerDetails;
    }
 
 
@@ -44,7 +46,7 @@ function addProduct(string memory _certificate,
 string memory _batchNumber,
 string memory _productionData,
 uint256 _producerId,
-string memory _truklipId,
+uint256 _truklipId,
 bytes32  uriInfo,
 address _producerAddress) onlyProducer(_producerId) public {
     productsMapping[_producerId] = Product({
@@ -54,26 +56,76 @@ address _producerAddress) onlyProducer(_producerId) public {
     producerAddress:_producerAddress,
     truklipId:_truklipId,
     uriInfo:uriInfo,
-    producerId:_producerId
-});
+    producerId:_producerId,
+    retailerDetail: Retailer({
+        truklipId:0,
+        recieveDate:0,
+        pickDate:0,
+        packagingBarcode:"",
+        otherDetails:"",
+        retailerAddress:address(0)}),
+    customerDetails:Customer({
+    klipId:0,
+    customerAddress:address(0), 
+    recieveDate:0,
+    invoiceNumber:0,
+    orderNumber:0,
+    otherDetails:""
+
+    })
+    });
 truklipId++;
 
 }
 
-function addRetailerDetails(uint256 _productId,uint256 _recievedate,uint256 _pickdate,uint256 _packingSpecification,string memory _packingbarcode) public {
-productsMapping[_productId].uriInfo =keccak256(abi.encode(_productId,_recievedate,_pickdate,_packingSpecification,_packingbarcode));
+
+struct Retailer{
+    uint256 truklipId;
+    uint256 recieveDate;
+    uint256 pickDate;
+    string packagingBarcode;
+    string otherDetails;
+    address retailerAddress;
 }
+
+
+mapping(uint256=>Retailer) retailerDetail;
+
+
+function addRetailerDetails(uint256 _truklipId,uint256 _recievedate,uint256 _pickdate,string memory _otherdetails,string memory _packingbarcode) public {
+productsMapping[_truklipId].retailerDetail = Retailer({
+    truklipId:_truklipId,
+    recieveDate:_recievedate,
+    pickDate:_pickdate,
+    packagingBarcode:_packingbarcode,
+    otherDetails:_otherdetails,
+    retailerAddress:msg.sender
+});
+}
+
+
+mapping(uint256=>address) customers;
+
+function addCustomer(uint256 _klipId,address _customerAddress) onlyAdmin public {
+    customers[_klipId]=_customerAddress;
+
+}
+
 
 struct Customer{
     uint256 klipId;
     address customerAddress; 
+    uint256 recieveDate;
+    uint256 invoiceNumber;
+    uint256 orderNumber;
+    string otherDetails;
 }
-mapping(uint256=>Customer) customersMapping;
+mapping(uint256=>Customer) customersDetails;
+
     
-function addCustomer(uint256 _klipId,address _customerAddress)onlyAdmin public {
 
 
-} 
+
 
 
 
