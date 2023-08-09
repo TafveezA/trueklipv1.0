cda52586a08df75a5969125df84dd12bfc42b85e
TafveezA <tafveez.alphadev@gmail.com>
2023-08-09 09:53:18 +0400

    removed error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -55,8 +55,11 @@ return next(new ErrorResponse('Invalid credentials',401))
             expires: new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
             httpOnly:true
              }
+        if(process.env.NODE_ENV ==='production'){
+            options.secure =true;
+        }
 
-            res.statusCode(statusCode).cookie('token',token,options).json({
+            res.status(statusCode).cookie('token',token,options).json({
                 success:true,
                 token:token})
     }
