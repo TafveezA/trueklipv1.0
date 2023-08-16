9b4875c9d57b6e5957efaa9326674186fb1dc5d3
TafveezA <tafveez.alphadev@gmail.com>
2023-08-16 11:25:27 +0400

    updates in controllers/smartcontract.js

--- server/controllers/smartcontract.js
+++ server/controllers/smartcontract.js
@@ -1,24 +1,41 @@
 
 const asyncHandler = require('../middleware/async')
+const dotenv = require('dotenv')
+
+dotenv.config({path:'../config/config.env'})
+
+
+//get account from the wallet
+
+const privateKey = process.env.PRIVATE_KEY; 
+
+
+
+
 
 
 const{Web3}=require("web3")
 const ABI= require("../config/validationABI.json")
-const ALCHEMY_RPC_URL_SEPOLIA="https://eth-sepolia.g.alchemy.com/v2/FOxH7918EsvBRVwTUVGk5gzhgE2UFoxJ"
-const CONTRACT_VALIDATION_ADDRESS="0xdeFd0014ead1D3aC3f172c27fb59c13609a8cED8"
+const ALCHEMY_RPC_URL_SEPOLIA=process.env.ALCHEMY_RPC_URL_SEPOLIA
+const CONTRACT_VALIDATION_ADDRESS=process.env.CONTRACT_VALIDATION_ADDRESS
 const web3 =new Web3(ALCHEMY_RPC_URL_SEPOLIA)
 const contract = new web3.eth.Contract(ABI,CONTRACT_VALIDATION_ADDRESS)
 
 
-//EOS Blockchain API Credentials
+//EOS Blockchain part 
+console.log( "Private Key",process.env.PRIVATE_KEY)
+console.log("EOS rpc url",process.env.EOS_RPC_URL)
+console.log("Supply chain smart contract on EOS",process.env.EOS_SUPPLYCHAIN_CONTRACT_ADDRESS)
 
-const privateKey = process.env.PRIVATE_KEY;
-    const EOS_RPC_URL='https://api.testnet.evm.eosnetwork.com/';
-    const EOS_PROVIDER = new Web3(EOS_RPC_URL)
 
-    const account = web3.eth.accounts.privateKeyToAccount(privateKey)
-    const eoscontractAddress = "0x192d2fC734D58fc2864A8E6F33a00361943DefBB" 
-    const supplyChainContract = new ethers.Contract(eoscontractAddress, abiSupplyChain, wallet);
+const EOS_RPC_URL=process.env.EOS_RPC_URL;
+const EOS_PROVIDER = new Web3(EOS_RPC_URL)
+const abiSupplyChain =ABI
+const eoscontractAddress = process.env.EOS_SUPPLYCHAIN_CONTRACT_ADDRESS
+
+
+console.log(privateKey.toUpperCase())
+const eosSupplyChainContract = new EOS_PROVIDER.eth.Contract(abiSupplyChain,eoscontractAddress);
 
 
 
@@ -44,6 +61,17 @@ exports.validateProduct = asyncHandler(async(req,res,next)=>{
             
 })
 
+exports.validateProductOnEOS = asyncHandler(async(req,res,next)=>{
+    const {id} = req.params
+    const result = await eosSupplyChainContract.methods.validate(id).call()
+    console.log(result)
+    res.status(200).json({
+        truklipId:id,
+        success:true,
+        valid:result,
+
+    })
+})
 
 
 
