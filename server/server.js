8b6f731d0f8005a5661ab1b9a0975e16b7608f10
TafveezA <tafveez.alphadev@gmail.com>
2023-08-22 17:42:46 +0400

    latest

--- server/server.js
+++ server/server.js
@@ -39,7 +39,7 @@ app.use(logger)
 app.use(errorHandler)
 app.use(bodyParser.json());
 app.use(cors({
-    origin: 'http://localhost:3000','*'
+    origin: '*'
   }));
 
 // To remove data using these defaults:
