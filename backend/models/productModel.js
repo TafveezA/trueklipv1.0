7f3311bc2f799079625cd76ec94965f2ad7bfb13
TafveezA <tafveez.alphadev@gmail.com>
2023-08-30 17:29:41 +0400

    modified product schema with distributor ,manufacturer and retailer schema

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -2,45 +2,196 @@ const mongoose = require('mongoose');
 const slugify = require('slugify');
 const geocoder = require('../utils/geocoder');
 
-const productSchema = mongoose.Schema({
-    klipid: {
+
+
+const distributorSchema = new mongoose.Schema({
+    truklipId: {
+        type: Number,
+        required: true,
+    },
+    shipmentDate: {
+        type: Number,
+        required: true,
+    },
+    orderNumber: {
+        type: Number,
+        required: true,
+    },
+    hssCode: {
+        type: Number,
+        required: true,
+    },
+    barcode: {
+        type: String,
+        required: true,
+    },
+});
+
+const customerSchema = new mongoose.Schema({
+    klipId: {
+        type: Number,
+        required: true,
+    },
+    customerAddress: {
+        type: String,
+        required: true,
+    },
+    recieveDate: {
+        type: Number,
+        required: true,
+    },
+    invoiceNumber: {
+        type: Number,
+        required: true,
+    },
+    orderNumber: {
+        type: Number,
+        required: true,
+    },
+    otherDetails: {
+        type: String,
+    },
+});
+
+const retailerSchema = new mongoose.Schema({
+    truklipId: {
+        type: Number,
+        required: true,
+    },
+    recieveDate: {
+        type: Number,
+        required: true,
+    },
+    pickDate: {
+        type: Number,
+        required: true,
+    },
+    packagingBarcode: {
+        type: String,
+        required: true,
+    },
+    otherDetails: {
+        type: String,
+    },
+    orderNumber: {
+        type: Number,
+        required: true,
+    },
+    retailerAddress: {
+        type: String,
+        required: true,
+    },
+});
+
+
+
+
+const productSchema = new mongoose.Schema(
+    {
+      klipid: {
         type: Number,
         required: [true, 'Please enter a valid integer truklipid']
-    },
-    batchnumber: {
+      },
+      batchnumber: {
         type: String,
         required: true,
         default: ""
-    },
-    mfgdate: {
+      },
+      mfgdate: {
         type: Date, 
         required: true 
-    },
-    expirydate: {
+      },
+      expirydate: {
         type: Date, 
         required: false,
         default: new Date(2025, 11, 17)
-    },
-    warranty: {
+      },
+      warranty: {
         type: Date, 
         required: false,
         default: new Date(2025, 11, 17)
-    },
-    description: {
+      },
+      description: {
         type: String,
         required: false,
         default: ""
-    },
-    user: {
+      },
+      user: {
         type: mongoose.Schema.ObjectId,
         ref: 'User',
         required: true
+      },
+      certificate: String,
+      productionData: String,
+      otherDetails: String,
+      truklipId: Number,
+      producerId: Number,
+      producerAddress: String,
+      isConsumable: Boolean,
+      distributorDetails: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Distributor'
+      },
+      retailerDetail: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Retailer'
+      },
+      customerDetails: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Customer'
+      }
+    },
+    {
+      timestamps: true
     }
+  );
+
+
+
+
+
+
+
+  // const productSchema = mongoose.Schema({
+//     klipid: {
+//         type: Number,
+//         required: [true, 'Please enter a valid integer truklipid']
+//     },
+//     batchnumber: {
+//         type: String,
+//         required: true,
+//         default: ""
+//     },
+//     mfgdate: {
+//         type: Date, 
+//         required: true 
+//     },
+//     expirydate: {
+//         type: Date, 
+//         required: false,
+//         default: new Date(2025, 11, 17)
+//     },
+//     warranty: {
+//         type: Date, 
+//         required: false,
+//         default: new Date(2025, 11, 17)
+//     },
+//     description: {
+//         type: String,
+//         required: false,
+//         default: ""
+//     },
+//     user: {
+//         type: mongoose.Schema.ObjectId,
+//         ref: 'User',
+//         required: true
+//     }
     
-}, 
-{
-    timestamps: true
-});
+// }, 
+// {
+//     timestamps: true
+// });
+
 
 // Pre-Save Middleware for Generating Slug
 productSchema.pre('save', function (next) {
--- backend/utils/pdfGenerator.js
+++ backend/utils/pdfGenerator.js
@@ -3,24 +3,15 @@ const { PDFDocument, rgb } = require('pdf-lib');
 
 async function generatePDF(jsonData) {
   try {
-    // Create a new PDF document
     const pdfDoc = await PDFDocument.create();
-
-    // Add a new page
     const page = pdfDoc.addPage([600, 400]);
-
-    // Embed JSON data onto the PDF page
     page.drawText(JSON.stringify(jsonData, null, 2), {
       x: 50,
       y: page.getHeight() - 100,
       size: 12,
       color: rgb(0, 0, 0),
     });
-
-    // Serialize the PDF
     const pdfBytes = await pdfDoc.save();
-
-    // Write the PDF to a file
     fs.writeFileSync('output.pdf', pdfBytes);
 
     console.log('PDF generated successfully.');
@@ -33,7 +24,6 @@ const jsonData = {
   name: 'John Doe',
   age: 30,
   email: 'john@example.com',
-  // ...other data
 };
 
 generatePDF(jsonData);
