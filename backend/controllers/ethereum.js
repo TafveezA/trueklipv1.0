1a6bc0c6f1ef201a133fcdc6e39a6d21118cb5ad
TafveezA <tafveez.alphadev@gmail.com>
2023-10-02 16:30:02 +0400

    added hardhat and modified eth apis

--- backend/controllers/ethereum.js
+++ backend/controllers/ethereum.js
@@ -4,19 +4,25 @@ const dotenv = require('dotenv')
 
 dotenv.config({path:'../config/config.env'})
 const CryptoJS = require('crypto-js');
+const ethers = require('ethers')
 
 const{Web3}=require("web3")
 const ABI= require("../config/validationABI.json")
+const { JsonRpcProvider } = require("@ethersproject/providers")
 
 
 const Product = require('../models/productModel.js')
 const ALCHEMY_RPC_URL_SEPOLIA=process.env.ALCHEMY_RPC_URL_SEPOLIA
-
+const PRIVATE_KEY=process.env.PRIVATE_KEY;
 const CONTRACT_VALIDATION_ADDRESS=process.env.CONTRACT_ADDRESS_VALIDATION
+
+
+console.log(ALCHEMY_RPC_URL_SEPOLIA)
+const alchemyProvider = new JsonRpcProvider(ALCHEMY_RPC_URL_SEPOLIA)
+const signer = new ethers.Wallet(PRIVATE_KEY,alchemyProvider)
 
 const web3 =new Web3(ALCHEMY_RPC_URL_SEPOLIA)
 const contract = new web3.eth.Contract(ABI,CONTRACT_VALIDATION_ADDRESS)
-const privateKey=process.env.PRIVATE_KEY;
 
 
 // @desc  get  validation of product
@@ -41,57 +47,43 @@ exports.validateProduct = asyncHandler(async(req,res,next)=>{
 })
 
 exports.addProductDetails = asyncHandler(async(req,res,next)=>{
-    
-    const jsonData = req.body
-    const truklipid=jsonData._truklipid;
-    const jsonString = JSON.stringify(jsonData);
-    const secretKey = "mySecretKey12345";
-    const encryptedData = CryptoJS.AES.encrypt(jsonString, secretKey).toString();
-    console.log("Private Key",privateKey)
+  const jsonData = req.body;
+  console.log(jsonData)
+  const truklipid = jsonData._truKlipId;
+  console.log("truklipid",truklipid)
+  const jsonString = JSON.stringify(jsonData);
+  const secretKey = "mySecretKey12345";
+  const encryptedData = CryptoJS.AES.encrypt(jsonString, secretKey).toString();
+  
+  console.log('Encrypted Data:', encryptedData);
+  
+  const validationContract = new ethers.Contract(CONTRACT_VALIDATION_ADDRESS, ABI, signer);
+  const maxPriorityFeePerGas = ethers.utils.parseUnits('10', 'gwei');
+  // const options = {
+  //   gasLimit: 25000, // Adjust this value as needed
+  // };
 
-console.log('Encrypted Data:', encryptedData);
+  const response = await validationContract.addProductEncryptedData(truklipid, encryptedData,options);
+  console.log(response)
 
-
-  const encodedData = contract.methods.addProductEncryptedData(truklipid, encryptedData).encodeABI();
-
-  // Sign the transaction
-  web3.eth.accounts.signTransaction(
-    {
-      to: contractAddress,
-      data: encodedData,
-      gas: '100000', // You may need to adjust the gas limit
-    },
-    privateKey
-  )
-    .then((signedTx) => {
-      // Send the signed transaction
-      web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-        .on('receipt', (receipt) => {
-          console.log('Transaction receipt:', receipt);
-          // Transaction has been mined
-        })
-        .on('error', (error) => {
-          console.error('Transaction error:', error);
-        });
-    })
-    .catch((error) => {
-      console.error('Transaction signing error:', error);
-    });
-
-// Decrypt the encrypted data back to a JSON string
-console.log('Decrypted JSON:', decryptedJsonData);
-const encryptedDataFromBlockchain = await contract.methods.getProductEncrytedData(truklipid).call()
-const bytes = CryptoJS.AES.decrypt(encryptedDataFromBlockchain, secretKey);
-const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
-
-console.log('Decrypted Data:', decryptedData);
-
-// Parse the JSON string back to a JSON object
-const decryptedJsonData = JSON.parse(decryptedData);
-
- res.status(200).json({
-        success:true,
-        valid:result,
-        dataretrived:decryptedJsonData
-    })
+  
+  // Decrypt the encrypted data back to a JSON string
+ 
+  const encryptedDataFromBlockchain = await validationContract.getProductEncrytedData(truklipid).call();
+  const bytes = CryptoJS.AES.decrypt(encryptedDataFromBlockchain, secretKey);
+  const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+  
+  console.log('Decrypted Data:', decryptedData);
+  
+  // Parse the JSON string back to a JSON object
+  const decryptedJsonData = JSON.parse(decryptedData);
+  
+  console.log('Decrypted JSON:', decryptedJsonData);
+  
+  res.status(200).json({
+    success: true,
+    valid: result, 
+    dataretrived: decryptedJsonData
+  });
+  
 })
