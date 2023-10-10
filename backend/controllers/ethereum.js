d81662d39940bc05b9133ce261f35e8e3b7e8d84
TafveezA <tafveez.alphadev@gmail.com>
2023-10-10 11:58:00 +0400

    added encryption data api

--- backend/controllers/ethereum.js
+++ backend/controllers/ethereum.js
@@ -3,11 +3,9 @@ const ErrorResponse = require('../utils/errorResponse')
 const dotenv = require('dotenv')
 
 dotenv.config({path:'../config/config.env'})
-
 const ethers = require('ethers')
 const { BigNumber } = require('bignumber.js');
-
-
+const CryptoJS = require('crypto-js');
 const ABI= require("../config/validationABI.json")
 
 
@@ -41,3 +39,34 @@ exports.addProductDetail = asyncHandler(async (req, res, next) => {
      transaction: tx
   });
 });
+
+
+exports.addProductDetails = asyncHandler(async(req,res,next)=>{
+  const jsonData = req.body;
+  console.log(jsonData)
+  const truklipid = jsonData._truKlipId;
+  console.log("truklipid",truklipid)
+  const jsonString = JSON.stringify(jsonData);
+  const secretKey = "mySecretKey12345";
+  const encryptedData = CryptoJS.AES.encrypt(jsonString, secretKey).toString();
+  
+  console.log('Encrypted Data:', encryptedData);
+
+  const txns = await contractInstance.addProductEncryptedData(truklipid,encryptedData)
+  await txns.wait()
+  const encryptedDataFromBlockchain = await contractInstance.getProductEncryptedData(truklipid);
+  const bytes = CryptoJS.AES.decrypt(encryptedDataFromBlockchain, secretKey);
+  const decryptedData = bytes.toString(CryptoJS.enc.Utf8);
+  
+  console.log('Decrypted Data:', decryptedData);
+  console.log('Decrypted data from blockchain',decryptedData)
+  console.log('Encrypted data from the blockchain',encryptedDataFromBlockchain)
+  const decryptedJsonData = JSON.parse(decryptedData);
+  console.log('Decrypted JSON:', decryptedJsonData);
+  
+  res.status(200).json({
+    success: true,
+    data:decryptedJsonData
+  });
+  
+})
