12bedf19ebbc77fd9c2d8a1ba37108f567beb2d1
TafveezA <tafveez.alphadev@gmail.com>
2023-10-17 16:29:03 +0400

    added NFT.sol and interact.js

--- backend/controllers/ethereum.js
+++ backend/controllers/ethereum.js
@@ -17,19 +17,17 @@ const CONTRACT_VALIDATION_ADDRESS=process.env.CONTRACT_ADDRESS_VALIDATION
 const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_RPC_URL_SEPOLIA);
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 const contractInstance = new ethers.Contract(CONTRACT_VALIDATION_ADDRESS, ABI, signer);
-
-
 console.log(ALCHEMY_RPC_URL_SEPOLIA)
 // INFURA_RPC_PROVIDER='https://sepolia.infura.io/v3/12d8279bb5a04bbabc47862d11820722'
 
 
-//let walletSigner = signer.connect(window.ethersProvider)
 
 
 
 
 
 
+//testing api for add product to blockchain
 exports.addProductDetail = asyncHandler(async (req, res, next) => {
   const tx = await contractInstance.addProduct('b');
   await tx.wait();
@@ -41,13 +39,42 @@ exports.addProductDetail = asyncHandler(async (req, res, next) => {
 });
 
 
+
+
+
+
+
+
+// @desc  get  validation of product
+// @route Get /api/eth/v1/validate
+// @access Public
+exports.validateProduct = asyncHandler(async(req,res,next)=>{
+  const {truklipid}= req.body
+  console.log(truklipid)
+  const result = await contractInstance.validationResult(truklipid).call();
+
+  console.log("Valid",result)
+                
+   res.status(200).json({
+    truklipId:truklipid,
+    success:true,
+    valid:result
+   })
+  
+})
+
+
+
+
+
+//this API is for encrypting the data and stroring to the ethereum blockchain
 exports.addProductDetails = asyncHandler(async(req,res,next)=>{
   const jsonData = req.body;
   console.log(jsonData)
   const truklipid = jsonData._truKlipId;
   console.log("truklipid",truklipid)
   const jsonString = JSON.stringify(jsonData);
-  const secretKey = "mySecretKey12345";
+  const secretKey =process.env.AES_ENCRYPTION_SECRET_KEY;
   const encryptedData = CryptoJS.AES.encrypt(jsonString, secretKey).toString();
   
   console.log('Encrypted Data:', encryptedData);
